05cf3483b0a191d98133d551209e0b3e37da825b
JMit-dev <darkkid8192004@gmail.com>
2026-06-01 15:22:39 -0400

    chore: move phoebus-plugin to its own repo (JMit-dev/shift-phoebus-plugin)

--- phoebus-plugin/src/main/java/org/phoebus/framework/autocomplete/ProposalProvider.java
+++
@@ -1,9 +0,0 @@
-package org.phoebus.framework.autocomplete;
-
-import java.util.List;
-
-// Stub matching the real org.phoebus.framework.autocomplete.ProposalProvider.
-public interface ProposalProvider {
-    String getName();
-    List<Proposal> lookup(String text);
-}
--- phoebus-plugin/src/main/java/org/phoebus/framework/spi/AppInstance.java
+++
@@ -1,8 +0,0 @@
-package org.phoebus.framework.spi;
-
-// Stub matching the real org.phoebus.framework.spi.AppInstance.
-public interface AppInstance {
-    AppDescriptor getAppDescriptor();
-    default void raise() {}
-    default void dispose() {}
-}
